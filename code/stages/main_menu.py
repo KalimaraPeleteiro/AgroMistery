2f8a27b5c2db8d098377d1268d19d5a1f4fed574
Kalimara_Peleteiro <kalimarapeleteiro@gmail.com>
2024-04-17 08:49:35 -0300

    Detalhe: Organizando Código

--- code/stages/main_menu.py
+++ code/stages/main_menu.py
@@ -1,24 +1,24 @@
 import pygame
 
 def main_menu(screen, screen_width, screen_height):
-    background = pygame.image.load("images/TitleScreenBg.png").convert()
+    background = pygame.image.load("images/MainTitle/TitleScreenBg.png").convert()
     background = pygame.transform.scale(background, (screen_width, screen_height))
 
-    title = pygame.image.load("images/GameTitle.png").convert_alpha()
+    title = pygame.image.load("images/MainTitle/GameTitle.png").convert_alpha()
     title_rect = title.get_rect()
     title_rect.centerx = screen_width / 2
     title_rect.top = 25
 
-    studio_logo = pygame.image.load("images/GameStudioLogo.png").convert_alpha()
+    studio_logo = pygame.image.load("images/MainTitle/GameStudioLogo.png").convert_alpha()
     studio_logo_rect = studio_logo.get_rect()
     studio_logo_rect.centerx = screen_width / 2
     studio_logo_rect.bottom = screen_height
 
-    title_start_button = pygame.image.load("images/TitleStartButton.png").convert_alpha()
+    title_start_button = pygame.image.load("images/MainTitle/TitleStartButton.png").convert_alpha()
     title_start_button_rect = title_start_button.get_rect()
     title_start_button_rect.center = (screen_width // 2, screen_height // 2 + 50)
 
-    title_exit_button = pygame.image.load("images/TitleExitButton.png").convert_alpha()
+    title_exit_button = pygame.image.load("images/MainTitle/TitleExitButton.png").convert_alpha()
     title_exit_button_rect = title_exit_button.get_rect()
     title_exit_button_rect.center = (screen_width // 2, screen_height // 2 + 150)
 
